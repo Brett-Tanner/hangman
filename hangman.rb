84f4c4ce79fc3fc8a49313fde5964403bacba2ce
Brett Tanner <bretttanner171@gmail.com>
2022-07-24 13:03:27 +0900

    Create outline

---
+++ hangman.rb
@@ -0,0 +1,29 @@
+# on launch, load the dictionary
+
+    # select all words with .length between 5 and 12
+
+    # select a random word from those
+
+    # create an array of equal length to the word, default value "_"
+
+    # print that array and ask the player for a guess
+
+        # also need to print some way of tracking guesses remaining and previously guessed letters
+
+# get player guess
+
+    # compare it to the randomly chosen word (if it was already guessed, tell them and ask again)
+
+    # reveal all letters that match in the guess array
+
+    # print, then ask for another guess
+
+    # when guesses exceed the allowed amount, end the game and reveal the word
+
+    # maybe save all played games to some kind of log file
+
+# allow saving at any time
+
+    # if the player enters "save" as their guess, save gamestate info to a file
+        # guesses remaining, current revealed letters, previous guesses, player name
+
